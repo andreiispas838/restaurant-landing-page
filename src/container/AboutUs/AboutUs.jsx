35fb529887290ba91e31d16272b903b2354938ff
Andrei Ispas <andreiispas838@yahoo.com>
2023-03-13 08:06:35 +0200

    navbar/header done

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => <div className='app__aboutus app__bg flex__center section__padding'>AboutUs</div>;
+
+export default AboutUs;
